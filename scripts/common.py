a1d025fd8eeb73aeb5c233c519915351d9180440
andy <andy.idiaghe@voyagecontrol.com>
2016-10-14 18:32:50 +0200

    ENH: Adding code

---
+++ scripts/common.py
@@ -0,0 +1,55 @@
+import tweepy
+
+from random import random
+
+from pymongo import MongoClient, DESCENDING
+
+from settings import *
+
+
+def mongo_connector():
+    client = MongoClient(MONGO_CLIENT)
+    db      = client.twitter
+    db.authenticate(AUTH_USR, AUTH_PWD)
+
+    return db
+
+db = mongo_connector()
+
+
+def insert_tweets(tweet_list):
+
+    tweets = []
+
+    for t in tweet_list:
+        if t.in_reply_to_screen_name is None:
+            print 'inserting tweets'
+            tweets.append(t)
+            db.tweets.insert({
+                'tweet_id': t.id,
+                'text': t.text,
+                'random': [random(), 0]}
+            )
+
+    return tweets
+
+
+def get_latest_tweets(screen_name):
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth)
+
+    last_tweet = list(db.tweets.find().sort('tweet_id', DESCENDING).limit(1))
+
+    print 'looking for tweets'
+
+    recent = api.user_timeline(screen_name=screen_name, count=200, since_id=last_tweet[0]['tweet_id'])
+
+    if recent:
+        insert_tweets(recent)
+    else:
+        print 'no new tweets!'
+
+
+def get_all_tweets(screen_name):
+    pass
--- scripts/daemon.py
+++ scripts/daemon.py
@@ -1,41 +1,9 @@
 ###########################################
-#	Daemon to populate the db periodically
+# Daemon to populate the db periodically
 ###########################################
-import tweepy
-from pymongo import MongoClient, ASCENDING, DESCENDING
-from random import random
-from local import settings
-from scripts.populate import get_specific
 
-client = MongoClient(settings.MONGO_CLIENT)
-db      = client.twitter
-db.authenticate(settings.AUTH_USR, settings.AUTH_PWD)
+import settings
+from common import get_tweets
 
-CONSUMER_KEY        = settings.CONSUMER_KEY
-CONSUMER_SECRET  = settings.CONSUMER_SECRET
-ACCESS_KEY               = settings.ACCESS_KEY
-ACCESS_SECRET         = settings.ACCESS_SECRET
-
-tweets                         = []
-
-last_tweet = list(db.tweets.find().sort('tweet_id', DESCENDING).limit(1))
-
-
-def get_all_tweets(screen_name):
-	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-	auth.set_access_token(access_key, access_secret)
-	api = tweepy.API(auth)
-
-	print 'looking for tweets'
-
-	recent = api.user_timeline(screen_name = screen_name,count=200,since_id=last_tweet[0]['tweet_id'])
-
-	
-	if recent:
-		get_specific(recent)
-	else:
-		print 'no new tweets!'
-	
 if __name__ == '__main__':
-	#pass in the username of the account you want to download
-	get_all_tweets(settings.TARGET_USR)
+    get_tweets(settings.TARGET_USR)
