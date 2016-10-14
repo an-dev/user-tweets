1faa031618a66d86e9f3173ff278a0cf4645f2f0
andy <andy.idiaghe@voyagecontrol.com>
2016-10-14 18:31:29 +0200

    NEW: Adding settings file

---
+++ settings.py
@@ -0,0 +1,14 @@
+# Settings file
+
+MONGO_CLIENT = None
+
+AUTH_USR = None
+AUTH_PWD = None
+
+CONSUMER_KEY     = None
+CONSUMER_SECRET  = None
+
+ACCESS_KEY       = None
+ACCESS_SECRET    = None
+
+TARGET_USR = None
